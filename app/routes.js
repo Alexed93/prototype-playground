664839e29c4251bd2a5c79fc38d91b03847d4a5a
Alexandra Edwards <alexed93@gmail.com>
2024-06-03 15:35:00 +0100

    Added VT snippets for seeing saved session data, and console logs

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,29 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
+////// Vicky Teinaki 
+////// https://www.vickyteinaki.com/blog/more-efficient-prototyping-with-the-gov-uk-prototype-kit-step-by-step/
+
+// Logging session data  
+router.use((req, res, next) => {    
+    const log = {  
+      method: req.method,  
+      url: req.originalUrl,  
+      data: req.session.data  
+    }  
+    console.log(JSON.stringify(log, null, 2))  
+   
+  next()  
+}) 
+
+// GET SPRINT NAME - useful for relative templates  
+router.use('/', (req, res, next) => {  
+    res.locals.currentURL = req.originalUrl; //current screen  
+    res.locals.prevURL = req.get('Referrer'); // previous screen
+  
+    console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
+  
+    next();  
+});
+
 // Add your routes here
