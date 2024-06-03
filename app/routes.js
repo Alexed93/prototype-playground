c08d2c54a52ca3e19c8be5b16b1cf4476e0093a7
Alexandra Edwards <alexed93@gmail.com>
2024-06-03 16:35:15 +0100

    Practicing of versioning

--- app/routes.js
+++ app/routes.js
@@ -25,12 +25,22 @@ router.use((req, res, next) => {
 
 // GET SPRINT NAME - useful for relative templates  
 router.use('/', (req, res, next) => {  
-    res.locals.currentURL = req.originalUrl; //current screen  
-    res.locals.prevURL = req.get('Referrer'); // previous screen
-  
-    console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );
-  
-    next();  
-});
+  res.locals.currentURL = req.originalUrl; //current screen  
+  res.locals.prevURL = req.get('Referrer'); // previous screen  
+  req.folder = req.originalUrl.split('/')[1]; //folder, e.g. 'current'  
+  req.subfolder = req.originalUrl.split('/')[2]; //sub-folder e.g. 'service'  
+  res.locals.folder = req.folder; // what folder the url is  
+  res.locals.subfolder = req.subfolder; // what subfolder the URL is in
+
+  console.log('folder : ' + res.locals.folder + ', subfolder : ' + res.locals.subfolder  );  
+  console.log('previous page is: ' + res.locals.prevURL + " and current page is " + req.url + " " + res.locals.currentURL );  
+next();  
+});  
 
 // Add your routes here
+
+
+// Start folder specific routes
+router.use('/sprint-1', require('./views/sprint-1/\_routes'));  
+// current sprint, remember to add older sprint when adding a new folder!  
+router.use('/current', require('./views/current/\_routes'));
